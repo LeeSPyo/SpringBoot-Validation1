6d08302a4b6b886635479d8cca71ef8ebc347724
LeeSPyo <oco141@naver.com>
2023-11-07 10:59:53 +0900

    Bean Validation - 수정에 적용

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -53,7 +53,7 @@ public class ValidationItemControllerV3 {
 	@PostMapping("/add")
 	public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult,
 			RedirectAttributes redirectAttributes) {
-		
+
 		// 특정 필드 예외가 아닌 전체 예외
 		if (item.getPrice() != null && item.getQuantity() != null) {
 			int resultPrice = item.getPrice() * item.getQuantity();
@@ -61,12 +61,12 @@ public class ValidationItemControllerV3 {
 				bindingResult.reject("totalPriceMin", new Object[] { 10000, resultPrice }, null);
 			}
 		}
-		
+
 		if (bindingResult.hasErrors()) {
 			log.info("errors={}", bindingResult);
 			return "validation/v3/addForm";
 		}
-		
+
 		// 성공 로직
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -82,7 +82,23 @@ public class ValidationItemControllerV3 {
 	}
 
 	@PostMapping("/{itemId}/edit")
-	public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+	public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+		
+		// 특정 필드 예외가 아닌 전체 예외
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			
+			int resultPrice = item.getPrice() * item.getQuantity();
+			
+			if (resultPrice < 10000) {
+				bindingResult.reject("totalPriceMin", new Object[] { 10000, resultPrice }, null);
+			}
+		}
+		
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
+			return "validation/v3/editForm";
+		}
+		
 		itemRepository.update(itemId, item);
 		return "redirect:/validation/v3/items/{itemId}";
 	}
